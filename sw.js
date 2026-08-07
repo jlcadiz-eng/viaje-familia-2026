a6408b0e1ad72160e4f74697fa78ee470fdf0e3b
jlcadiz-eng <jlcadiz@gmail.com>
2026-08-08 00:56:01 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'viaje-familia-v1';
+const CACHE_NAME = 'viaje-familia-v2';
 const FILES_TO_CACHE = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
-  );
+    );
   self.skipWaiting();
 });
 
@@ -12,13 +12,20 @@ self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
       Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
-    )
-  );
+                       )
+    );
   self.clients.claim();
 });
 
+// Red primero (para ver siempre lo ultimo), y si no hay conexion usa la copia guardada.
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request))
-  );
+    fetch(event.request)
+    .then((response) => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+      return response;
+    })
+    .catch(() => caches.match(event.request))
+    );
 });
